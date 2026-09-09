b57d5653741ea0f02b96e0d906f33a3153949025
John Dexter Ramos <ramosjohndexter7@gmail.com>
2026-09-09 09:56:54 +0800

    Allow Jeddah Supabase images

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,12 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
+        hostname: 'qamdcpgwkveikddemvhz.supabase.co',
+        port: '',
+        pathname: '/storage/v1/object/public/**',
+      },
+      {
+        protocol: 'https',
         hostname: 'msoomcjzzudibiyezclj.supabase.co',
         port: '',
         pathname: '/storage/v1/object/public/**',
